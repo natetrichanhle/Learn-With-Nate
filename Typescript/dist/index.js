299412f4e345980102cdd620a1496084dc6df576
Nate Trichanh <natetrichanh@gmail.com>
2022-11-17 02:36:47 -0800

    Typescript fundamentals

--- Typescript/dist/index.js
+++ Typescript/dist/index.js
@@ -1,3 +1,11 @@
 "use strict";
-let age = 20;
+let sales = 123456789;
+let course = 'Typescript';
+let is_published = true;
+let level;
+let numbers = [];
+let user = [1, 'Nate'];
+;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,4 +1,48 @@
-let age: number = 20;
-if (age < 50) 
-    age += 10;
-console.log(age)
+let sales = 123_456_789;
+let course = 'Typescript';
+let is_published = true;
+let level;
+
+// function render(document) {
+//     console.log(document);
+// }
+// need to declare the type in a function
+
+// ARRAYS
+let numbers: number[] = [];
+// numbers.forEach(n => n.) itellisense (code completion with dot notation)
+
+// TUPLES
+let user: [number, string] =  [1, 'Nate'];
+// user[1]. itellisense (code completion with dot notation)
+
+// ENUMS
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+// PascalCase
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+console.log(mySize);
+
+// FUNCTIONS
+function calculateTax(income: number, taxYear = 2022): number {
+    if (taxYear < 2022)
+        return income * 1.2;
+    return income * 1.3;
+}
+
+calculateTax(10_000);
+
+// OBJECTS
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = { 
+    id: 1, 
+    name: 'Nate',
+    retire: (date:Date) => {
+        console.log(date);
+    }
+};
